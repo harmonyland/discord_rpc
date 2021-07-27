9db53574d54afd8bf941f32905fbfc2fb79a9eb5
DjDeveloperr <djsandhu2005@gmail.com>
2021-07-27 10:36:52 +0530

    feat: ipc path resolvation, finding correct ipc, macOS support, better reading

--- mod.ts
+++ mod.ts
@@ -1,18 +1,11 @@
-import { varnumBytes } from "https://deno.land/std@0.95.0/encoding/binary.ts";
-
-function encode(type: number, payload: string) {
-  const encoded = new TextEncoder().encode(payload);
-  const _type = varnumBytes(type, { endian: "little" });
-  const _len = varnumBytes(encoded.length, { endian: "little" });
-
-  return new Uint8Array([..._type, ..._len, ...encoded]);
-}
-
-async function read(ipc: Deno.Conn | undefined) {
-  if (!ipc) return;
-  const buf = new Uint8Array(512);
-  await ipc.read(buf);
-  return new TextDecoder().decode(buf);
+function encode(op: number, payloadString: string) {
+  const payload = new TextEncoder().encode(payloadString);
+  const data = new Uint8Array(4 + 4 + payload.byteLength);
+  const view = new DataView(data.buffer);
+  view.setInt32(0, op, true);
+  view.setInt32(4, payload.byteLength, true);
+  data.set(payload, 8);
+  return data;
 }
 
 interface Activity {
@@ -43,15 +36,53 @@ interface Activity {
   }[];
 }
 
+export enum OpCode {
+  HANDSHAKE,
+  FRAME,
+  CLOSE,
+  PING,
+  PONG,
+}
+
 const _ipcHandle = Symbol("[[ipc]]");
 
+function getIPCPath(id: number) {
+  if (id < 0 || id > 9) throw new RangeError(`IPC ID must be between 0-9`);
+
+  let prefix, suffix = `discord-ipc-${id}`;
+  if (Deno.build.os === "windows") {
+    prefix = `\\\\?\\pipe\\`;
+  } else {
+    prefix = Deno.env.get("XDG_RUNTIME_DIR") ?? Deno.env.get("TMPDIR") ??
+      Deno.env.get("TMP") ?? Deno.env.get("TEMP") ?? "/tmp";
+  }
+
+  return `${prefix}${suffix}`;
+}
+
+async function findIPC(id = 0): Promise<string> {
+  const path = getIPCPath(id);
+  try {
+    await Deno.open(path).then((e) => e.close());
+    return path;
+  } catch (e) {
+    return findIPC(id + 1);
+  }
+}
+
 export async function createClient(): Promise<RichPresence> {
+  const path = await findIPC();
   const client = Object.create(RichPresence.prototype);
+
+  // NOTE(DjDeveloperr): Should we remove this condition since OS-specific logic is handled
+  // in the findIPCPath function? This would maybe just start working once unix sockets are
+  // enabled on Windows.
+
   // Open a IPC connection
   if (Deno.build.os !== "windows") {
     // Unix
     client[_ipcHandle] = await Deno.connect({
-      path: "/run/user/1000/discord-ipc-0",
+      path,
       transport: "unix",
     });
   } else {
@@ -65,6 +96,8 @@ export async function createClient(): Promise<RichPresence> {
   return client;
 }
 
+// NOTE(DjDeveloperr): We should probably use a better name for this,
+// Discord IPC is much more than just a way to set Rich Presence.
 export class RichPresence {
   [_ipcHandle]?: Deno.Conn;
 
@@ -75,20 +108,41 @@ export class RichPresence {
   async login(client_id: string) {
     const payload = JSON.stringify({ v: "1", client_id });
     await this[_ipcHandle]?.write(encode(0, payload));
-    await read(this[_ipcHandle]);
+    await this.#read();
+  }
+
+  #header = new Uint8Array(8);
+  #headerView = new DataView(this.#header.buffer);
+
+  // TODO(DjDeveloperr): Keep reading in a loop until it's closed since Discord IPC
+  // is capable of emitting events too, not just write then read.
+  async #read() {
+    if (await this[_ipcHandle]?.read(this.#header) !== 8) return;
+    const op = this.#headerView.getInt32(0, true);
+    const payloadLength = this.#headerView.getInt32(4, true);
+    const data = new Uint8Array(payloadLength);
+    if (await this[_ipcHandle]?.read(data) !== payloadLength) return;
+    const payload = new TextDecoder().decode(data);
+    return JSON.parse(payload);
+  }
+
+  async send(op: OpCode, payload: any) {
+    const nonce = crypto.randomUUID();
+    if (typeof payload === "object" && payload !== null) payload.nonce = nonce;
+    const data = encode(op, JSON.stringify(payload));
+    await this[_ipcHandle]?.write(data);
+    return nonce;
   }
 
   async setActivity(activity: Activity) {
-    const payload = JSON.stringify({
+    await this.send(OpCode.FRAME, {
       cmd: "SET_ACTIVITY",
       args: {
         pid: Deno.pid,
         activity,
       },
-      nonce: await crypto.randomUUID(),
     });
-    await this[_ipcHandle]?.write(encode(1, payload));
-    await read(this[_ipcHandle]);
+    await this.#read();
   }
 
   async close() {
